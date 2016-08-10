57c05b586a0f38321029361a606bbaa013783808
Sofia <sofia.ringstrom@bizkithavas.se>
2016-08-10 08:47:12 +0200

    Styled blog template

---
+++ home.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name: Blog
+ *
+ * @package rngstrm
+ * @subpackage JE Theme
+ * @since 1.0.0
+ */
+?>
+
+<?php get_header(); ?>
+<article id="blog-template">
+
+	<section>
+		<div class="row">
+			<div class="column small-12">
+				<?php
+					if ( have_posts() ) {
+						while ( have_posts() ) {
+							the_post();
+							//the_title();
+							//the_content();
+							the_post_thumbnail();
+						}
+					}
+				?>
+			</div>
+		</div>
+
+	</section>
+
+</article>
+
+<?php get_footer(); ?>
